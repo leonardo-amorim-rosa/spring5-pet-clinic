149dc6d548051ddabf0502f9a8b71572e5bdeb8a
Leonardo Rosa <leonardo.rosa@mobicare.com.br>
2023-02-25 15:47:39 -0300

    Initial project add

---
+++ src/test/java/br/com/mx8sistemas/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mx8sistemas.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
